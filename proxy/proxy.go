ea875a5e4b2e947c8f17908046dcb4cc6d85895c
Lawrence Finn <laf2019@columbia.edu>
2019-04-01 21:44:01 -0400

    refactor

--- proxy/proxy.go
+++ proxy/proxy.go
@@ -122,6 +122,190 @@ func (p *Proxy) returnPool(pl *pool.Pool, read bool) {
 	}
 }
 
+func ListenForMessage(client net.Conn) error {
+	message, length, err := connect.Receive(client)
+	if err != nil {
+		return err
+	}
+	if length < 1 {
+		return io.EOF
+	}
+	log.Infof("Incoming message %s %v", message, message)
+
+	var someLength uint32
+
+	commandCompleteSelect := make([]byte, 5)
+	commandCompleteSelect[0] = 'C'
+	commandCompleteSelect = append(commandCompleteSelect, []byte("SELECT")...)
+	commandCompleteSelect = append(commandCompleteSelect, byte(0))
+	binary.BigEndian.PutUint32(commandCompleteSelect[1:5], uint32(len("SELECT"))+5)
+
+	commandCompleteSet := make([]byte, 5)
+	commandCompleteSet[0] = 'C'
+	commandCompleteSet = append(commandCompleteSet, []byte("SET")...)
+	commandCompleteSet = append(commandCompleteSet, byte(0))
+	binary.BigEndian.PutUint32(commandCompleteSet[1:5], uint32(len(commandCompleteSet)-1))
+
+	okMessage := make([]byte, 6)
+	okMessage[0] = 'Z'
+	someLength = 5
+	binary.BigEndian.PutUint32(okMessage[1:5], someLength)
+	okMessage[5] = 'I'
+
+	switch message[0] {
+	case 'P':
+		queryLength := binary.BigEndian.Uint32(message[1:5])
+		buffer := protocol.NewMessageBuffer(message)
+		buffer.Seek(5)
+		preparedStmtName, _ := buffer.ReadString()
+		query, _ := buffer.ReadString()
+		numParams, _ := buffer.ReadInt16()
+		fmt.Printf("Looks like a parse query of length %d \"%s\" \"%s\" %d\n", queryLength, preparedStmtName, query, numParams)
+		bindByte, _ := buffer.ReadByte()
+		if bindByte == 'B' {
+			bindLength, _ := buffer.ReadInt32()
+			fmt.Printf("Bind byte? %s %d\n", string(bindByte), bindLength)
+			buffer.ReadBytes(int(bindLength - 4))
+		}
+		describeByte, _ := buffer.ReadByte()
+		if describeByte == 'D' {
+			dataLength, _ := buffer.ReadInt32()
+			describeType, _ := buffer.ReadByte()
+			describeName, _ := buffer.ReadString()
+			fmt.Printf("Data byte? %s %d %s \"%s\"\n", string(describeByte), dataLength, string(describeType), describeName)
+		}
+		executeByte, _ := buffer.ReadByte()
+		if executeByte == 'E' {
+			executeLength, _ := buffer.ReadInt32()
+			executeName, _ := buffer.ReadString()
+			maxRows, _ := buffer.ReadInt32()
+			fmt.Printf("Execute byte %s %d \"%s\" %d\n", string(executeByte), executeLength, executeName, maxRows)
+		}
+		flushByte, _ := buffer.ReadByte()
+		if flushByte == 'H' {
+			flushLength, _ := buffer.ReadInt32()
+			fmt.Printf("Flush byte %s %d\n", string(flushByte), flushLength)
+		}
+		syncByte, _ := buffer.ReadByte()
+		if syncByte == 'S' {
+			syncLength, _ := buffer.ReadInt32()
+			fmt.Printf("Sync byte %s %d\n", string(syncByte), syncLength)
+		}
+
+		parseMessage := make([]byte, 5)
+		parseMessage[0] = '1'
+		someLength = 4
+		binary.BigEndian.PutUint32(parseMessage[1:5], someLength)
+		bindMessage := make([]byte, 5)
+		bindMessage[0] = '2'
+		someLength = 4
+		binary.BigEndian.PutUint32(bindMessage[1:5], someLength)
+
+		if query == "SELECT character_value, version() FROM INFORMATION_SCHEMA.SQL_IMPLEMENTATION_INFO WHERE implementation_info_id = '17' or implementation_info_id = '18'" {
+			initValuesColumns := make([]byte, 67)
+			initValuesColumns[0] = 'T'
+			binary.BigEndian.PutUint32(initValuesColumns[1:5], 66)
+			binary.BigEndian.PutUint16(initValuesColumns[5:7], 2)
+			copy(initValuesColumns[7:], []byte{99, 104, 97, 114, 97, 99, 116, 101, 114, 95, 118, 97, 108, 117, 101, 0, 0, 0, 67, 73, 0, 4, 0, 0, 4, 19, 255, 255, 0, 1, 0, 3, 0, 0, 118, 101, 114, 115, 105, 111, 110, 0, 0, 0, 0, 0, 0, 0, 0, 0, 4, 19, 255, 255, 0, 1, 0, 3, 0, 0})
+
+			initValuesData := make([]byte, 145)
+			initValuesData[0] = 'D'
+			binary.BigEndian.PutUint32(initValuesData[1:5], 144)
+			binary.BigEndian.PutUint16(initValuesData[5:7], 2)
+			copy(initValuesData[7:], []byte{0, 0, 0, 10, 80, 111, 115, 116, 103, 114, 101, 83, 81, 76, 0, 0, 0, 120, 80, 111, 115, 116, 103, 114, 101, 83, 81, 76, 32, 56, 46, 48, 46, 50, 32, 111, 110, 32, 105, 54, 56, 54, 45, 112, 99, 45, 108, 105, 110, 117, 120, 45, 103, 110, 117, 44, 32, 99, 111, 109, 112, 105, 108, 101, 100, 32, 98, 121, 32, 71, 67, 67, 32, 103, 99, 99, 32, 40, 71, 67, 67, 41, 32, 51, 46, 52, 46, 50, 32, 50, 48, 48, 52, 49, 48, 49, 55, 32, 40, 82, 101, 100, 32, 72, 97, 116, 32, 51, 46, 52, 46, 50, 45, 54, 46, 102, 99, 51, 41, 44, 32, 82, 101, 100, 115, 104, 105, 102, 116, 32, 49, 46, 48, 46, 54, 55, 53, 52,})
+
+			initValuesData2 := make([]byte, 145)
+			initValuesData2[0] = 'D'
+			binary.BigEndian.PutUint32(initValuesData2[1:5], 144)
+			binary.BigEndian.PutUint16(initValuesData2[5:7], 2)
+			copy(initValuesData2[7:], []byte{0, 0, 0, 10, 48, 56, 46, 48, 48, 46, 48, 48, 48, 50, 0, 0, 0, 120, 80, 111, 115, 116, 103, 114, 101, 83, 81, 76, 32, 56, 46, 48, 46, 50, 32, 111, 110, 32, 105, 54, 56, 54, 45, 112, 99, 45, 108, 105, 110, 117, 120, 45, 103, 110, 117, 44, 32, 99, 111, 109, 112, 105, 108, 101, 100, 32, 98, 121, 32, 71, 67, 67, 32, 103, 99, 99, 32, 40, 71, 67, 67, 41, 32, 51, 46, 52, 46, 50, 32, 50, 48, 48, 52, 49, 48, 49, 55, 32, 40, 82, 101, 100, 32, 72, 97, 116, 32, 51, 46, 52, 46, 50, 45, 54, 46, 102, 99, 51, 41, 44, 32, 82, 101, 100, 115, 104, 105, 102, 116, 32, 49, 46, 48, 46, 54, 55, 53, 52,})
+
+			fmt.Printf("Sending %s %v\n", parseMessage, parseMessage)
+			fmt.Printf("Sending %s %v\n", bindMessage, bindMessage)
+			fmt.Printf("Sending %s %v\n", initValuesColumns, initValuesColumns)
+			fmt.Printf("Sending %s %v\n", initValuesData, initValuesData)
+			fmt.Printf("Sending %s %v\n", initValuesData2, initValuesData2)
+			fmt.Printf("Sending %s %v\n", commandCompleteSelect, commandCompleteSelect)
+			fmt.Printf("Sending %s %v\n", okMessage, okMessage)
+			_, err = connect.Send(client, parseMessage)
+			_, err = connect.Send(client, bindMessage)
+			_, err = connect.Send(client, initValuesColumns)
+			_, err = connect.Send(client, initValuesData)
+			_, err = connect.Send(client, initValuesData2)
+			_, err = connect.Send(client, commandCompleteSelect)
+			//_, err = connect.Send(client, noMessage)
+			_, err = connect.Send(client, okMessage)
+		} else if query == "SELECT supported_value FROM INFORMATION_SCHEMA.SQL_SIZING WHERE sizing_id = 34 or sizing_id = 30 or sizing_id = 31 or sizing_id = 10005 or sizing_id = 32 or sizing_id = 35 or sizing_id = 107 or sizing_id = 97 or sizing_id = 99 or sizing_id = 100 or sizing_id = 101" {
+			initValuesColumns := make([]byte, 41)
+			initValuesColumns[0] = 'T'
+			binary.BigEndian.PutUint32(initValuesColumns[1:5], 40)
+			binary.BigEndian.PutUint16(initValuesColumns[5:7], 1)
+			copy(initValuesColumns[7:], []byte{115, 117, 112, 112, 111, 114, 116, 101, 100, 95, 118, 97, 108, 117, 101, 0, 0, 0, 67, 88, 0, 3, 0, 0, 0, 23, 0, 4, 255, 255, 255, 255, 0, 0})
+
+			initValuesData := make([]byte, 1)
+			initValuesData[0] = 'D'
+			// just removed first 68 / D
+			initValuesData = append(initValuesData, []byte{0, 0, 0, 11, 0, 1, 0, 0, 0, 1, 48, 68, 0, 0, 0, 11, 0, 1, 0, 0, 0, 1, 48, 68, 0, 0, 0, 14, 0, 1, 0, 0, 0, 4, 49, 54, 54, 52, 68, 0, 0, 0, 14, 0, 1, 0, 0, 0, 4, 49, 54, 48, 48, 68, 0, 0, 0, 13, 0, 1, 0, 0, 0, 3, 49, 50, 55, 68, 0, 0, 0, 13, 0, 1, 0, 0, 0, 3, 49, 50, 55, 68, 0, 0, 0, 13, 0, 1, 0, 0, 0, 3, 49, 50, 55, 68, 0, 0, 0, 13, 0, 1, 0, 0, 0, 3, 49, 50, 55, 68, 0, 0, 0, 13, 0, 1, 0, 0, 0, 3, 49, 50, 55, 68, 0, 0, 0, 13, 0, 1, 0, 0, 0, 3, 49, 50, 55, 68, 0, 0, 0, 13, 0, 1, 0, 0, 0, 3, 49, 50, 55}...)
+
+			fmt.Printf("Sending %s %v\n", parseMessage, parseMessage)
+			fmt.Printf("Sending %s %v\n", bindMessage, bindMessage)
+			fmt.Printf("Sending %s %v\n", initValuesColumns, initValuesColumns)
+			fmt.Printf("Sending %s %v\n", initValuesData, initValuesData)
+			fmt.Printf("Sending %s %v\n", commandCompleteSelect, commandCompleteSelect)
+			fmt.Printf("Sending %s %v\n", okMessage, okMessage)
+
+			_, err = connect.Send(client, parseMessage)
+			_, err = connect.Send(client, bindMessage)
+			_, err = connect.Send(client, initValuesColumns)
+			_, err = connect.Send(client, initValuesData)
+			_, err = connect.Send(client, commandCompleteSelect)
+			_, err = connect.Send(client, okMessage)
+		} else if query == "select version()" {
+			initValuesColumns := make([]byte, 33)
+			initValuesColumns[0] = 'T'
+			binary.BigEndian.PutUint32(initValuesColumns[1:5], 32)
+			binary.BigEndian.PutUint16(initValuesColumns[5:7], 1)
+			copy(initValuesColumns[7:], []byte{118, 101, 114, 115, 105, 111, 110, 0, 0, 0, 0, 0, 0, 0, 0, 0, 4, 19, 255, 255, 0, 1, 0, 3, 0, 0})
+
+			initValuesData := make([]byte, 1)
+			initValuesData[0] = 'D'
+			// just removed first 68 / D
+			initValuesData = append(initValuesData, []byte{0, 0, 0, 130, 0, 1, 0, 0, 0, 120, 80, 111, 115, 116, 103, 114, 101, 83, 81, 76, 32, 56, 46, 48, 46, 50, 32, 111, 110, 32, 105, 54, 56, 54, 45, 112, 99, 45, 108, 105, 110, 117, 120, 45, 103, 110, 117, 44, 32, 99, 111, 109, 112, 105, 108, 101, 100, 32, 98, 121, 32, 71, 67, 67, 32, 103, 99, 99, 32, 40, 71, 67, 67, 41, 32, 51, 46, 52, 46, 50, 32, 50, 48, 48, 52, 49, 48, 49, 55, 32, 40, 82, 101, 100, 32, 72, 97, 116, 32, 51, 46, 52, 46, 50, 45, 54, 46, 102, 99, 51, 41, 44, 32, 82, 101, 100, 115, 104, 105, 102, 116, 32, 49, 46, 48, 46, 54, 55, 53, 52}...)
+
+			fmt.Printf("Sending %s %v\n", parseMessage, parseMessage)
+			fmt.Printf("Sending %s %v\n", bindMessage, bindMessage)
+			fmt.Printf("Sending %s %v\n", initValuesColumns, initValuesColumns)
+			fmt.Printf("Sending %s %v\n", initValuesData, initValuesData)
+			fmt.Printf("Sending %s %v\n", commandCompleteSelect, commandCompleteSelect)
+			fmt.Printf("Sending %s %v\n", okMessage, okMessage)
+
+			_, err = connect.Send(client, parseMessage)
+			_, err = connect.Send(client, bindMessage)
+			_, err = connect.Send(client, initValuesColumns)
+			_, err = connect.Send(client, initValuesData)
+			_, err = connect.Send(client, commandCompleteSelect)
+			_, err = connect.Send(client, okMessage)
+		}
+	case 'X':
+		client.Close()
+	case 'Q':
+		setMessage := make([]byte, 5)
+		setMessage[0] = 'S'
+		setMessage = append(setMessage, []byte("application_name")...)
+		setMessage = append(setMessage, byte(0))
+		setMessage = append(setMessage, []byte("JDBC-1.2.20.1043")...)
+		setMessage = append(setMessage, byte(0))
+		binary.BigEndian.PutUint32(setMessage[1: 5], uint32(len(setMessage) - 1))
+		fmt.Printf("Sending %s %v\n", setMessage, setMessage)
+		fmt.Printf("Sending %s %v\n", commandCompleteSet, commandCompleteSet)
+		fmt.Printf("Sending %s %v\n", okMessage, okMessage)
+		_, err = connect.Send(client, setMessage)
+		_, err = connect.Send(client, commandCompleteSet)
+		_, err = connect.Send(client, okMessage)
+	}
+	return nil
+}
+
 // HandleConnection handle an incoming connection to the proxy
 func (p *Proxy) HandleConnection(client net.Conn) {
 	/* Get the client startup message. */
@@ -141,6 +325,7 @@ func (p *Proxy) HandleConnection(client net.Conn) {
 	log.Infof("Version %s",  version)
 	log.Infof("Message %v length %d", message, length)
 	if version == protocol.SSLRequestCode {
+		log.Info("Blurp")
 		sslResponse := protocol.NewMessageBuffer([]byte{})
 
 		/* Determine which SSL response to send to client. */
@@ -249,70 +434,19 @@ func (p *Proxy) HandleConnection(client net.Conn) {
 	okMessage[5] = 'I'
 	fmt.Printf("Sending %s %v\n", okMessage, okMessage)
 	_, err = connect.Send(client, okMessage)
-	message, length, err = connect.Receive(client)
-	fmt.Printf("ZI response %s %v\n", message, message)
 
-	if message[0] == 'P' {
-		queryLength := binary.BigEndian.Uint32(message[1:5])
-		buffer := protocol.NewMessageBuffer(message)
-		buffer.Seek(5)
-		preparedStmtName, _ := buffer.ReadString()
-		//buffer.Seek(5 + len(preparedStmtName))
-		query, _ := buffer.ReadString()
-		numParams, _ := buffer.ReadInt16()
-		fmt.Printf("Looks like a parse query of length %d \"%s\" \"%s\" %d\n", queryLength, preparedStmtName, query,  numParams)
-		bindByte, _ := buffer.ReadByte()
-		if bindByte == 'B' {
-			bindLength, _ := buffer.ReadInt32()
-			fmt.Printf("Bind byte? %s %d\n", string(bindByte), bindLength)
-			buffer.ReadBytes(int(bindLength - 4))
-		}
-		describeByte, _ := buffer.ReadByte()
-		if describeByte == 'D' {
-			dataLength, _ := buffer.ReadInt32()
-			describeType, _ := buffer.ReadByte()
-			describeName, _ := buffer.ReadString()
-			fmt.Printf("Data byte? %s %d %s \"%s\"\n", string(describeByte), dataLength, string(describeType), describeName)
-		}
-		executeByte, _ := buffer.ReadByte()
-		if executeByte == 'E' {
-			executeLength, _ := buffer.ReadInt32()
-			executeName, _ := buffer.ReadString()
-			maxRows, _ := buffer.ReadInt32()
-			fmt.Printf("Execute byte %s %d \"%s\" %d\n", string(executeByte), executeLength, executeName, maxRows)
-		}
-		flushByte, _ := buffer.ReadByte()
-		if flushByte == 'H' {
-			flushLength, _ := buffer.ReadInt32()
-			fmt.Printf("Flush byte %s %d\n", string(flushByte), flushLength)
-		}
-		syncByte, _ := buffer.ReadByte()
-		if syncByte == 'S' {
-			syncLength, _ := buffer.ReadInt32()
-			fmt.Printf("Sync byte %s %d\n", string(syncByte), syncLength)
+
+	for {
+		msgErr := ListenForMessage(client)
+		if msgErr == io.EOF {
+			break
+		} else if msgErr != nil {
+			log.Errorf("Some error %v", msgErr)
+			break
 		}
 	}
 
-	noMessage := make([]byte, 5)
-	noMessage[0] = 'n'
-	someLength = 4
-	binary.BigEndian.PutUint32(noMessage[1:5], someLength)
 
-	parseMessage := make([]byte, 5)
-	parseMessage[0] = '1'
-	someLength = 4
-	binary.BigEndian.PutUint32(parseMessage[1:5], someLength)
-	bindMessage := make([]byte, 5)
-	bindMessage[0] = '2'
-	someLength = 4
-	binary.BigEndian.PutUint32(bindMessage[1:5], someLength)
-	fmt.Printf("Sending %s %v\n", parseMessage, parseMessage)
-	_, err = connect.Send(client, parseMessage)
-	_, err = connect.Send(client, bindMessage)
-	_, err = connect.Send(client, noMessage)
-	_, err = connect.Send(client, okMessage)
-	message, length, err = connect.Receive(client)
-	fmt.Printf("ZI response %s %v", message, message)
 
 	authenticated := true
 	/* If the client could not authenticate then go no further. */
